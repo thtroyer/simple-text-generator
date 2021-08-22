235c3b2ed043184ff37d1bf27f373b4e580e4f48
Tom Troyer <tom.troyer@gmail.com>
2021-08-22 15:09:12 -0400

    Adding new models to help estimate ETA of training

---
+++ simpletextgenerator/models/worklist.py
@@ -0,0 +1,54 @@
+import abc
+from typing import List, Tuple
+
+
+class WorkList:
+    def __init__(self, items_to_complete: Tuple):
+        self._progress_current_task = 0
+        self._items_to_complete = items_to_complete
+        self._current_item_index = 0
+        self._training_rate = 0
+        self._generating_rate = 0
+
+    def get_time_estimate(self):
+        total_seconds = 0
+        current_task = True
+        for work_item in self._items_to_complete[self._current_item_index:]:
+            modifier = 1
+            if current_task:
+                modifier = (1 - self._progress_current_task)
+                current_task = False
+
+            total_seconds += modifier * work_item.get_time_estimate(self._generating_rate, self._training_rate)
+        return total_seconds
+
+    def advance_index(self):
+        self._current_item_index += 1
+
+    def set_training_rate(self, training_rate: float):
+        self._training_rate = training_rate
+
+    def set_generating_rate(self, generating_rate: float):
+        self._generating_rate = generating_rate
+
+    def set_progress_on_current_work_item(self, progress: float):
+        self._progress_current_task = progress
+
+
+class WorkItem:
+    def __init__(self, items_to_complete: int):
+        self._items_to_complete = items_to_complete
+
+    @abc.abstractmethod
+    def get_time_estimate(self, generation_rate: float = 0.0, training_rate: float = 0.0):
+        pass
+
+
+class TrainingWorkItem(WorkItem):
+    def get_time_estimate(self, generation_rate: float = 0.0, training_rate: float = 0.0):
+        return self._items_to_complete / training_rate
+
+
+class GeneratingWorkItem(WorkItem):
+    def get_time_estimate(self, generation_rate: float = 0.0, training_rate: float = 0.0):
+        return self._items_to_complete / generation_rate
---
+++ tests/models/test_worklist.py
@@ -0,0 +1,70 @@
+from simpletextgenerator.models.worklist import WorkItem, WorkList, TrainingWorkItem, GeneratingWorkItem
+
+
+def test_trainingWorkItem():
+    training_work_item = TrainingWorkItem(50)
+    assert (training_work_item.get_time_estimate(training_rate=50)) == 1
+    assert (training_work_item.get_time_estimate(training_rate=1.0)) == 50
+    assert (training_work_item.get_time_estimate(training_rate=1.2)) == 50 / 1.2
+
+
+def test_generatingWorkItem():
+    generating_work_item = GeneratingWorkItem(50)
+    assert (generating_work_item.get_time_estimate(generation_rate=50)) == 1
+    assert (generating_work_item.get_time_estimate(generation_rate=1.0)) == 50
+    assert (generating_work_item.get_time_estimate(generation_rate=1.2)) == 50 / 1.2
+
+
+def test_Worklist_get_time_estimate():
+    workList = WorkList((
+        GeneratingWorkItem(50),
+        TrainingWorkItem(200)
+    ))
+    workList.set_training_rate(2)
+    workList.set_generating_rate(1)
+
+    assert workList.get_time_estimate() == 150
+
+
+def test_Worklist_get_time_estimate_progress():
+    workList = WorkList((
+        TrainingWorkItem(200),
+        GeneratingWorkItem(50)
+    ))
+    workList.set_training_rate(2)
+    workList.set_generating_rate(1)
+
+    assert workList.get_time_estimate() == 150
+
+    workList.set_progress_on_current_work_item(0.5)
+
+    assert workList.get_time_estimate() == 100
+
+    workList.set_progress_on_current_work_item(0.75)
+
+    assert workList.get_time_estimate() == 75
+
+
+def test_Worklist_advance_index():
+    workList = WorkList((
+        GeneratingWorkItem(50),
+        GeneratingWorkItem(50),
+        TrainingWorkItem(100),
+        TrainingWorkItem(100)
+    ))
+    workList.set_training_rate(1)
+    workList.set_generating_rate(1)
+
+    assert workList.get_time_estimate() == 300
+    workList.advance_index()
+    assert workList.get_time_estimate() == 250
+    workList.advance_index()
+    assert workList.get_time_estimate() == 200
+    workList.advance_index()
+    assert workList.get_time_estimate() == 100
+    workList.advance_index()
+    assert workList.get_time_estimate() == 0
+
+    # make sure advancing too far doesn't cause problem
+    workList.advance_index()
+    assert workList.get_time_estimate() == 0
