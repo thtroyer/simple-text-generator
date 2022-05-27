d284348cc3174b1347fe1ad622f2099f689ede2d
Tom Troyer <tom.troyer@gmail.com>
2022-05-26 22:24:27 -0400

    Adjusting setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='simple-text-generator',
     version='0.3.0',
-    packages=['simpletextgenerator'],
+    # packages=['simpletextgenerator'],
+    package=find_packages(),
     package_dir={'': 'tests'},
     url='github.com/thtroyer/simple-text-generator',
     license='MIT',
     author='thtroyer',
     author_email='tom.troyer@gmail.com',
-    description=''
+    description='',
+    install_requires=['textgenrnn', 'tensorflow==2.4.2', 'chevron', 'PyYAML', 'pipfile']
 )
